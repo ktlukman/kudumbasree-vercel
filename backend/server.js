3c23f8203cdf56989a5b93f637622022c667ce8a
luqman <ktlukman@gmail.com>
2025-09-03 10:59:24 +0530

    kud

--- backend/server.js
+++ backend/server.js
@@ -1,112 +1,65 @@
 const express = require("express");
 const cors = require("cors");
-const Database = require("better-sqlite3");
+const mysql = require("mysql");
 const fileUpload = require("express-fileupload");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(fileUpload());
-
-// Connect to SQLite DB
-//const db = new Database("./users.sqlite", { verbose: console.log });
-
-const db = new Database("libsql://kudumbasree-vercel-icfg-a3h790hpuuivixz0rg1mr6lp.aws-us-east-1.turso.io", { verbose: console.log });
-
-
-// GET all users
+const db = mysql.createConnection({
+  host: "p3plzcpnl504695.prod.phx3.secureserver.net",
+  user: "luqman",
+  password: "wpzn}_A%p,^G",
+  database: "kudumbasree"
+  /* host: "localhost",
+  user: "root",
+  password: "",
+  database: "kudumbasree" */
+})
 app.get("/", (req, res) => {
-  try {
-    const stmt = db.prepare("SELECT * FROM users");
-    const users = stmt.all();
-    res.json(users);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
+  const sql = "SELECT * FROM users";
+  db.query(sql, (err, data) => {
+    if (err) return res.json("Error");
+    return res.json(data);
+  });
 });
-
-// POST - Create a new user
-app.post("/create", (req, res) => {
-  try {
-    const stmt = db.prepare(`INSERT INTO users 
-      (FirstName, LastName, Mobile, DateOfBirth, Proffession, Address, DateOfJoin, Position, WeeklyFee, AttendenceDays, Status)
-      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`);
-    
-    const info = stmt.run(
-      req.body.firstname,
-      req.body.lastname,
-      req.body.mobile,
-      req.body.dateofbirth,
-      req.body.proffession,
-      req.body.address,
-      req.body.dateofjoin,
-      req.body.position,
-      req.body.weeklyfee,
-      req.body.attendence,
-      req.body.status
-    );
-    
-    res.json({ id: info.lastInsertRowid });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
-
-// PUT - Update user
-app.put("/update/:id", (req, res) => {
-  try {
-    console.log("Update request body:", req.body);
-    console.log("Update user ID:", req.params.id);
-
-    const stmt = db.prepare(`UPDATE users SET 
-      FirstName = ?, LastName = ?, Mobile = ?, DateOfBirth = ?, Proffession = ?, Address = ?, 
-      DateOfJoin = ?, Position = ?, WeeklyFee = ?, AttendenceDays = ?, Status = ?
-      WHERE ID = ?`);
-
-    const info = stmt.run(
-      req.body.firstname,
-      req.body.lastname,
-      req.body.mobile,
-      req.body.dateofbirth,
-      req.body.proffession,
-      req.body.address,
-      req.body.dateofjoin,
-      req.body.position,
-      req.body.weeklyfee,
-      req.body.attendence,
-      req.body.status,
-      req.params.id
-    );
-
-    res.json({ changes: info.changes });
-  } catch (err) {
-    console.error("PUT error:", err.message);
-    res.status(500).json({ error: err.message });
-  }
-});
-
-// DELETE user
-app.delete("/users/:id", (req, res) => {
-  try {
-    const stmt = db.prepare("DELETE FROM users WHERE ID = ?");
-    const info = stmt.run(req.params.id);
-    res.json({ changes: info.changes });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
-
-// GET one user by ID
-app.get("/lister/:id", (req, res) => {
-  try {
-    const stmt = db.prepare("SELECT * FROM users WHERE ID = ?");
-    const user = stmt.get(req.params.id);
-    res.json(user);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
-
+app.post('/create', (req, res) => {
+  const sql = "INSERT INTO users (FirstName, LastName, Mobile, DateOfBirth, Proffession, Address, DateOfJoin, Position, WeeklyFee, AttendenceDays, Status) VALUES (?)";
+  const values = [req.body.firstname, req.body.lastname, req.body.mobile, req.body.dateofbirth, req.body.proffession, req.body.address, req.body.dateofjoin, req.body.position, req.body.weeklyfee, req.body.attendence, req.body.status]
+	db.query(sql, [values], (err, data) => {
+    if (err) return res.json("Error");
+    return res.json(data);
+  })
+})
+app.put('/update/:id', (req, res) => {
+  const sql = "update users set FirstName = ?, LastName = ?, Mobile = ?, DateOfBirth = ?, Proffession = ?, Address = ?, DateOfJoin = ?, Position = ?, WeeklyFee = ?, AttendenceDays = ?, Status = ? where ID = ?";
+  const values = [req.body.firstname, req.body.lastname, req.body.mobile, req.body.dateofbirth, req.body.proffession, req.body.address, req.body.dateofjoin, req.body.position, req.body.weeklyfee, req.body.attendence, req.body.status]
+  const id = req.params.id;
+  db.query(sql, [...values, id], (err, data) => {
+    if (err) return res.json("Error");
+    return res.json(data);
+  })
+})
+app.delete('/users/:id', (req, res) => {
+  const sql = "DELETE FROM users WHERE ID = ?";
+  const id = req.params.id;
+  db.query(sql, [id], (err, data) => {
+    if (err) return res.json("Error");
+    return res.json(data);
+  })
+})
+app.get('/lister/:id', (req, res) => {
+  const sql = "SELECT * FROM users WHERE ID = ?";
+  const id = req.params.id;
+  db.query(sql, [id], (err, data) => {
+	  for(let i=0; i<data.length;i++){
+		  return res.json(data[i]);
+	  }
+    //if (err) return res.json("Error");
+    //return res.json(data);
+  })
+})
 app.listen(8081, () => {
-  console.log("Server running on http://localhost:8081");
+  console.log("listening");
 });
